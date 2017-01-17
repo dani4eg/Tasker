2457a2996bd700a5c119aa239a6b613acd2f8359
darthMilash <dani4eg92@gmail.com>
2017-01-17 09:24:17 +0200

    add full project

--- src/TaskIO.java
+++ src/TaskIO.java
@@ -1,6 +1,4 @@
-import java.io.File;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 
 /**
  * Created by dani4 on 16.01.2017.
@@ -22,4 +20,22 @@ public class TaskIO {
     public static void readBinary(TaskList tasks, File file) {
 
     }
+
+    public static void write(TaskList tasks, Writer out) {
+
+    }
+
+    public static void read(TaskList tasks, Reader in) {
+
+    }
+
+    public static void  writeText(TaskList tasks, File file) {
+
+    }
+
+    public static void readText(TaskList tasks, File file) {
+
+    }
+
+
 }
